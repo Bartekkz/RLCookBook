05ddd9601a7ee9c0b6735cc719e62ea5efa20dfe
bartekkz <bartek.szmelczynski@gmail.com>
2020-03-31 01:01:08 +0200

    init Qlearning directory

---
+++ algorithms/Qlearning/Agent.py
@@ -0,0 +1,78 @@
+import numpy as np
+from collections import namedtuple
+
+pair = namedtuple("pair", field_names=["state", "action"])
+Experience = named
+
+
+class Agent:
+    """
+    Agent will play frozen lake game
+
+    Parameters:
+    -----------
+    lr: float
+        learning rate
+    gamma: float
+        discount factor for future rewards
+    n_actions: int
+        number of actions in given environment(game)
+    n_states: int
+        number of states in given environment(game)
+    eps_start: float
+        start value of epsilon
+    eps_end: float
+        end value of epsilon
+    eps_dec: float
+        how much epsilon will decay
+    """
+
+    def __init__(self, lr: float, gamma: float, n_actions: int, n_states: int, eps_start: float, eps_end: float, eps_dec: float) -> None:
+        self.lr = lr
+        self.gamma = gamma
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.epsilon = eps_start
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
+        self.Q = {}
+        self._init_q_values()
+
+    def _init_q_values(self) -> None:
+        """
+        Init q values for all <state, action> pairs to 0
+        """
+        for state in range(self.n_states):
+            for action in range(self.n_actions):
+                self.Q[pair(state, action)] = 0
+
+    def choose_action(self, state: int) -> int:
+        """
+        In this example we are gonna use epsilon greedy
+        strategy for exploration vs exploitation problem
+        """
+        if np.random.random() > self.epsilon:
+            action = np.random.choice([action for action in n_actions])
+        else:
+            actions = np.array([self.Q[(state, action)]
+                                for action in range(self.n_actions)])
+            action = np.argmax(actions)
+        return action
+
+    def decrement_epsilon(self) -> None:
+        """
+        Updates epsilon accroding to eps_dec value 
+        """
+        self.epsilon = self.epsilon * \
+            self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+    def update_q_table(self, state: int, action: int, reward: float, next_state: int) -> None:
+        next_actions = np.array([self.Q[next_state, action] for action in range(n_actions)])
+        next_max_action = np.argmax(next_actions)
+
+        self.Q[(state, action)]
+
+
+if __name__ == "__main__":
+    agent = Agent(1, 1, 10, 2, 10, 10, 10)
+    agent.choose_action(1)
