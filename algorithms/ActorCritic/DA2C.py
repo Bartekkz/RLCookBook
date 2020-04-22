f5b828da9512842001b1550fc41fdcedab1d6d3e
bartekkz <bartek.szmelczynski@gmail.com>
2020-04-22 23:27:00 +0200

    update a2c

--- algorithms/ActorCritic/DA2C.py
+++ algorithms/ActorCritic/DA2C.py
@@ -31,11 +31,41 @@ class ActorCritic(nn.Module):
         y = F.relu(self.l2(y))
         actor = F.log_softmax(self.actor_lin1(y), dim=0)
         c = F.relu(self.l3(y.detach())) # detach from differential graph
-        critic = nn.tanh(self.critic_lin1(c))
+        critic = torch.tanh(self.critic_lin1(c))
         return actor, critic
 
 
 def worker(t, worker_model, counter, params):
     worker_env = gym.make("CartPole-v1")
     worker_env.reset()
+    worker_opt = optim.Adam(lr=1e-4, params=worker_model.parameters())
+    worker_opt.zero_grad()
+    for i in range(params['epochs']):
+        worker_opt.zero_grad()
+        values, logprobs, rewards = 1, 1, 1 
+
+
+def run_episode(worker_env, worker_model):
+    state = torch.from_numpy(worker_env.env.state).float()
+    print(state)
+    values, logprobs, rewards = [], [], []
+    done = False
+    j = 0
+    while (done == False):
+        j += 1
+        policy, value = worker_model(state)
+        values.append(value)
+        logits = policy.view(-1)
+        action_dist = torch.distributions.Categorical(logits=logits)
+        action = action_dist.sample()
+        print(action)
+        exit(0)
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1")
+    env.reset()
+    ac = ActorCritic()
+    run_episode(env, ac)
+        
+        
 
